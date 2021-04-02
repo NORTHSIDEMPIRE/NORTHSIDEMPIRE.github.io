41635ffb00873cda00dfda5647bdb0e6dd2faf13
NORTHSIDEMPIRE <empirenorthside@gmail.com>
2021-04-02 19:38:00 +0800

    edit index.js

--- event-handler/javascript/index.js
+++ event-handler/javascript/index.js
@@ -7,3 +7,14 @@ let elNamaPengguna = document.getElementById("namaPengguna");
 elNamaPengguna.addEventListener("keyup", function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan");
+    let password_val = elKataLaluan.value;
+    if(password_val.length > 5){
+        alert("Password Check...OK!");
+
+    } else{
+        alert("Password too short");
+    }
+}
