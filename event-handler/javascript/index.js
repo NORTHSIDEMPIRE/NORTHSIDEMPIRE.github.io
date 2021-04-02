950322e1e001475de0a6364b410b250ff2242b86
NORTHSIDEMPIRE <empirenorthside@gmail.com>
2021-04-02 20:22:41 +0800

    edited js

--- event-handler/javascript/index.js
+++ event-handler/javascript/index.js
@@ -3,14 +3,14 @@ return _val.toUppercase();
 
 }
 
-let elNamaPengguna = document.getElementById("namaPengguna");
-elNamaPengguna.addEventListener("keyup", function(){
-    elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
+let elnamaPengguna = document.getElementById("namaPengguna");
+elnamaPengguna.addEventListener("keyup", function(){
+    elnamaPengguna.value = transformUppercase(elnamaPengguna.value);
 });
 
 function checkPassword(){
-    let elKataLaluan = document.getElementById("kataLaluan");
-    let password_val = elKataLaluan.value;
+    let elkataLaluan = document.getElementById("kataLaluan");
+    let password_val = elkataLaluan.value;
     if(password_val.length > 5){
         alert("Password Check...OK!");
 
